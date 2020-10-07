ca639743eed79cfdd4e24d4078fafa8872b1849d
Olga Murina <olgamurina92@gmail.com>
2020-10-07 19:09:21 +0300

    feat: add createDreamTeam function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let dreamTeamName = [];
+
+  if (Array.isArray(members)) {
+    members.forEach(member => {
+      if (typeof member === 'string') {
+          member = member.trim();
+          dreamTeamName.push(member[0].toUpperCase());
+      };
+    });
+  } else {
+    return false;
+  };
+
+  dreamTeamName = dreamTeamName.sort().join('');
+
+  return dreamTeamName.length > 0 ? dreamTeamName : false;
 };
