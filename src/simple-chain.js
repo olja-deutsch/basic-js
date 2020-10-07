5aec98b58c3741bc23995bdc0e124d72c634dee1
Olga Murina <olgamurina92@gmail.com>
2020-10-07 21:55:59 +0300

    feat: add object chainMaker with methods

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (String(value) !== '') {
+      this.chain.push(value);
+    } else {
+        this.chain.push('');
+    };
+
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (Number.isInteger(position) && position >= 0 && position < this.chain.length) {
+      this.chain.splice(position - 1, 1);
+    } else {
+        this.chain = [];
+        throw new Error();
+    };
+
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let result = '';
+
+    this.chain.forEach((item, index) => {
+      if (index === (this.chain.length - 1)) {
+        result += `( ${item} )`
+      } else {
+        result += `( ${item} )~~`
+      };
+    });
+
+    this.chain = [];
+
+    return result;
   }
 };
 
