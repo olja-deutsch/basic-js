e8442afdec482a2a2134d74d034023369065fb9a
Olga Murina <olgamurina92@gmail.com>
2020-10-27 13:13:22 +0300

    feat: add repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+  str = String(str);
+  const repeatTimes = options.repeatTimes !== undefined ? Number(options.repeatTimes) : 0;
+  const separator = options.separator !== undefined ? String(options.separator) : '+';
+  const addition = options.addition !== undefined ? String(options.addition) : '';
+  const additionRepeatTimes = options.additionRepeatTimes !== undefined ? Number(options.additionRepeatTimes) : 0;
+  const additionSeparator = options.additionSeparator !== undefined ? String(options.additionSeparator) : '|';
+  let resultString = '';
+  let additionString = additionRepeatTimes === 0 ? addition : '';
+  let stringToRepeat = '';
+
+  for (let i = 0; i < additionRepeatTimes; i++) {
+    if (i === (additionRepeatTimes - 1)) {
+      additionString += addition;
+      break;
+    }
+
+    additionString += addition + additionSeparator;
+  }
+
+  stringToRepeat = str + additionString;
+
+  for (let i = 0; i < repeatTimes; i++) {
+    if (i === (repeatTimes - 1)) {
+      resultString += stringToRepeat;
+      break;
+    }
+
+    resultString += stringToRepeat + separator;
+  }
   
+  resultString = repeatTimes === 0 ? stringToRepeat : resultString;
+
+  return resultString;
+};
